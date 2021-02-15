0d680a6a16e991cd46d342a953cbaba8984d9704
SWLeee <tkddnjs0687@naver.com>
2021-02-16 02:22:03 +0900

    react and express start

---
+++ client/src/App.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+class App extends Component {
+  state = { users: [] }
+  
+
+  componentDidMount(){
+
+    fetch('/users')
+    .then(res => res.json())
+    .then(users => this.setState({ users }));
+
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Users</h1>
+        <ul>
+        
+          {this.state.users.map(user =>
+            <li key={user.id}> {user.username} </li>        
+          )}
+
+        </ul>
+        {this.state.users.map(user =>{
+            console.log(user.username);
+          })}
+          
+      </div>
+
+    );
+  }
+}
+
+export default App;
